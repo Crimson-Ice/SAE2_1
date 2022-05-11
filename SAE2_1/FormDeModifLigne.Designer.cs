9500c976f5b0a72f1aaf16dee1f4a02b2750fb71
Swann_Bali <Swannbld@gmail.com>
2022-05-11 16:01:06 +0200

    drag and drop

--- SAE2_1/FormDeModifLigne.Designer.cs
+++ SAE2_1/FormDeModifLigne.Designer.cs
@@ -29,62 +29,30 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.lbl4 = new System.Windows.Forms.Label();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
-            this.lbl3 = new System.Windows.Forms.Label();
             this.txt_new_nom = new System.Windows.Forms.TextBox();
             this.lbl2 = new System.Windows.Forms.Label();
             this.lbl_titre = new System.Windows.Forms.Label();
             this.cmd1 = new System.Windows.Forms.Button();
             this.cmd2 = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.new_arret_btn = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
             this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.panel1.Controls.Add(this.lbl4);
-            this.panel1.Controls.Add(this.comboBox1);
-            this.panel1.Controls.Add(this.lbl3);
             this.panel1.Controls.Add(this.txt_new_nom);
             this.panel1.Controls.Add(this.lbl2);
             this.panel1.Controls.Add(this.lbl_titre);
             this.panel1.Location = new System.Drawing.Point(36, 26);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(729, 168);
+            this.panel1.Size = new System.Drawing.Size(729, 126);
             this.panel1.TabIndex = 0;
-            // 
-            // lbl4
-            // 
-            this.lbl4.AutoSize = true;
-            this.lbl4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl4.Location = new System.Drawing.Point(404, 118);
-            this.lbl4.Name = "lbl4";
-            this.lbl4.Size = new System.Drawing.Size(185, 25);
-            this.lbl4.TabIndex = 5;
-            this.lbl4.Text = "Couleur de la ligne :";
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(173, 122);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(57, 24);
-            this.comboBox1.TabIndex = 4;
-            // 
-            // lbl3
-            // 
-            this.lbl3.AutoSize = true;
-            this.lbl3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl3.Location = new System.Drawing.Point(15, 122);
-            this.lbl3.Name = "lbl3";
-            this.lbl3.Size = new System.Drawing.Size(151, 25);
-            this.lbl3.TabIndex = 3;
-            this.lbl3.Text = "Nombre d\'arret :";
             // 
             // txt_new_nom
             // 
-            this.txt_new_nom.Location = new System.Drawing.Point(259, 65);
+            this.txt_new_nom.Location = new System.Drawing.Point(271, 72);
             this.txt_new_nom.Name = "txt_new_nom";
             this.txt_new_nom.Size = new System.Drawing.Size(164, 22);
             this.txt_new_nom.TabIndex = 2;
@@ -93,7 +61,7 @@
             // 
             this.lbl2.AutoSize = true;
             this.lbl2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbl2.Location = new System.Drawing.Point(15, 62);
+            this.lbl2.Location = new System.Drawing.Point(27, 69);
             this.lbl2.Name = "lbl2";
             this.lbl2.Size = new System.Drawing.Size(238, 25);
             this.lbl2.TabIndex = 1;
@@ -115,9 +83,9 @@
             this.cmd1.Cursor = System.Windows.Forms.Cursors.Default;
             this.cmd1.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmd1.ForeColor = System.Drawing.Color.White;
-            this.cmd1.Location = new System.Drawing.Point(36, 341);
+            this.cmd1.Location = new System.Drawing.Point(36, 391);
             this.cmd1.Name = "cmd1";
-            this.cmd1.Size = new System.Drawing.Size(146, 81);
+            this.cmd1.Size = new System.Drawing.Size(101, 47);
             this.cmd1.TabIndex = 1;
             this.cmd1.Text = "Retour";
             this.cmd1.UseVisualStyleBackColor = false;
@@ -136,6 +104,27 @@
             this.cmd2.UseVisualStyleBackColor = false;
             this.cmd2.Click += new System.EventHandler(this.cmd2_Click);
             // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AllowDrop = true;
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(46, 192);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(207, 176);
+            this.flowLayoutPanel1.TabIndex = 3;
+            this.flowLayoutPanel1.DragEnter += new System.Windows.Forms.DragEventHandler(this.flowLayoutPanel1_DragEnter);
+            this.flowLayoutPanel1.DragOver += new System.Windows.Forms.DragEventHandler(this.flowLayoutPanel1_DragOver);
+            // 
+            // new_arret_btn
+            // 
+            this.new_arret_btn.Location = new System.Drawing.Point(393, 192);
+            this.new_arret_btn.Name = "new_arret_btn";
+            this.new_arret_btn.Size = new System.Drawing.Size(145, 26);
+            this.new_arret_btn.TabIndex = 4;
+            this.new_arret_btn.Text = "nouveau_arret";
+            this.new_arret_btn.UseVisualStyleBackColor = true;
+            this.new_arret_btn.MouseDown += new System.Windows.Forms.MouseEventHandler(this.new_arret_btn_MouseDown);
+            // 
             // FormDeModifLigne
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
@@ -143,6 +132,8 @@
             this.BackgroundImage = global::SAE2_1.Properties.Resources._6637000_simple_fond_bleu_abstraite_pour_la_conception_;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.new_arret_btn);
+            this.Controls.Add(this.flowLayoutPanel1);
             this.Controls.Add(this.cmd2);
             this.Controls.Add(this.cmd1);
             this.Controls.Add(this.panel1);
@@ -158,13 +149,12 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label lbl4;
-        private System.Windows.Forms.ComboBox comboBox1;
-        private System.Windows.Forms.Label lbl3;
         private System.Windows.Forms.TextBox txt_new_nom;
         private System.Windows.Forms.Label lbl2;
         private System.Windows.Forms.Label lbl_titre;
         private System.Windows.Forms.Button cmd1;
         private System.Windows.Forms.Button cmd2;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button new_arret_btn;
     }
 }
--- SAE2_1/FormDeModifLigne.cs
+++ SAE2_1/FormDeModifLigne.cs
@@ -13,8 +13,11 @@ namespace SAE2_1
 {
     public partial class FormDeModifLigne : Form
     {
-
+        public List<string> Arret = new List<string>();
         public MySqlConnection connexion = new MySqlConnection("database=baseb1; server=10.1.139.236; user id=b1; pwd=nouveau_mdp");
+
+        private bool appuie = false;
+        private bool dedans = false;
         public FormDeModifLigne()
         {
             InitializeComponent();
@@ -27,7 +30,30 @@ namespace SAE2_1
 
         private void FormDeModifLigne_Load(object sender, EventArgs e)
         {
+            new_arret_btn.Width = flowLayoutPanel1.Width-25;
+            new_arret_btn.Height = 23;
             lbl_titre.Text = $"Modification de la ligne : {this.Text}";
+
+            
+            
+            MySqlCommand arretcom = new MySqlCommand("select Arret.nom_arret from Correspondance,Ligne,Arret where Correspondance.id_ligne = Ligne.id_ligne and Correspondance.id_arret = Arret.id_arret and Ligne.nom_ligne =" + '\u0022' + this.Text + '\u0022' + " order by rang_arret_ligne;", connexion);
+
+            connexion.Open();
+
+            MySqlDataReader mysqlread = arretcom.ExecuteReader(CommandBehavior.CloseConnection);
+
+            while (mysqlread.Read())
+            {
+                string nom = mysqlread.GetString(0);
+                Arret.Add(nom);
+
+            }
+
+            spawnButton(Arret);
+
+           connexion.Close();
+
+            
         }
 
         private void cmd2_Click(object sender, EventArgs e)
@@ -48,7 +74,7 @@ namespace SAE2_1
                 while (MyReader.Read())
                 {
                 }
-
+                MyReader.Close();
                 connexion.Close();
             }
 
@@ -56,5 +82,46 @@ namespace SAE2_1
             this.Close();
             
         }
+
+        private void spawnButton(List<string> Arret)
+        {
+            for (int i = 0; i < Arret.Count(); i++)
+            {
+                Button button = new Button();
+                //button.Click += buttonArretClick;
+                button.Text = Arret[i];
+                button.Width = flowLayoutPanel1.Width - 25;
+                button.Tag = i;
+                button.Location = new Point(0, button.Height * i);
+                this.flowLayoutPanel1.Controls.Add(button);
+
+                
+            }
+        }
+
+        private void new_arret_btn_MouseDown(object sender, MouseEventArgs e)
+        {
+            
+            new_arret_btn.DoDragDrop(new_arret_btn, DragDropEffects.Move);
+            appuie = true;
+        }
+
+        private void flowLayoutPanel1_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Move;
+            if (!appuie )
+            {
+                flowLayoutPanel1.Controls.Add(new_arret_btn);
+            }
+            
+        }
+
+        private void flowLayoutPanel1_DragOver(object sender, DragEventArgs e)
+        {
+            dedans = true;
+        }
+
+
+        
     }
 }
